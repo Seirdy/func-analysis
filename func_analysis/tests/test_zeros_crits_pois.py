7e117d065de9a1872c06377212f4dc91561a7acc
Rohan Kumar <m2700879@nwytg.net>
2018-12-19 05:21:54 -0800

    Tests: Combine test_zeros with test_crits_pois

--- func_analysis/tests/test_zeros_crits_pois.py
+++ func_analysis/tests/test_zeros_crits_pois.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 # pylint: disable=comparison-with-callable
-"""Tests crit- and poi-finding algos in func_analysis.
+"""Tests zero-, crit-, and poi-finding algos in func_analysis.
 
 This deliberately uses a function requiring a high degree of precision
 """
@@ -12,6 +12,33 @@ from tests import constants
 
 from .._analysis_classes import AnalyzedFunc
 from .helpers import assert_output_lessthan, mpf_assert_allclose, typecheck_zcp
+
+
+def test_trig_func_has_correct_zeros(analyzed_trig_func):
+    """Test the correctness of analyzed_trig_func.zeros."""
+    typecheck_zcp(analyzed_trig_func.zeros)
+    # approximate accuracy
+    np.testing.assert_allclose(
+        np.float64(analyzed_trig_func.zeros),
+        constants.TRIG_FUNC_ZEROS,
+        rtol=constants.EPSILON_1,
+    )
+    # Does the function evaluate to 0 at its zeros?
+    assert_output_lessthan(
+        func=analyzed_trig_func.func,
+        x_vals=analyzed_trig_func.zeros,
+        max_y=3.5692e-19,
+    )
+
+
+def test_parabola_has_correct_zeros(analyzed_parab):
+    """Check that analyzed_parab.zeros returns correct value."""
+    np.testing.assert_equal(analyzed_parab.zeros, np.array([-2, 2]))
+
+
+def test_incdecfunc_has_correct_zeros(analyzed_incdecfunc):
+    """Test analyzed_incdecfunc.zeros returns correct value."""
+    assert analyzed_incdecfunc.zeros == [-1]
 
 
 def test_trig_func_has_correct_crits(analyzed_trig_func):
