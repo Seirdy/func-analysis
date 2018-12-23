40587edbdccb14465cb863fa0e85bb333bb05556
Rohan Kumar <m2700879@nwytg.net>
2018-12-22 19:13:38 -0800

    Move analyzed_func to its own dedicated submodule

---
+++ func_analysis/analyzed_func/__init__.py
@@ -0,0 +1,7 @@
+"""Module to create AnalyzedFunc.
+
+AnalyzedFunc is a class to contain a function and properties
+to describe its behavior.
+"""
+__all__ = ["AnalyzedFunc"]
+from .analyzed_func import AnalyzedFunc
