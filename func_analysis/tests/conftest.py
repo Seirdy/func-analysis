ac98f208e4f78e3c9ca24d44163297fdd9e91dbd
Rohan Kumar <m2700879@nwytg.net>
2018-12-26 12:35:36 -0800

    Testing: new test: zeros stay the same when no zeros are provided.

--- func_analysis/tests/conftest.py
+++ func_analysis/tests/conftest.py
@@ -12,24 +12,29 @@ from func_analysis.tests.funcs_to_analyze import (
     trig_func,
 )
 
-trig_func_args: Mapping = {
-    "func": trig_func,
-    "x_range": (-47.05, -46.3499),
-    "zeros_wanted": 21,
-    "crits_wanted": 21,
-    "known_zeros": [-47.038289673236127, -46.406755885040056],
-}
+
+@pytest.fixture
+def trig_func_args() -> Mapping:
+    """Arguments to contruct an AnalyzedFunc from trig_func."""
+    return {
+        "func": trig_func,
+        "x_range": (-47.05, -46.3499),
+        "zeros_wanted": 21,
+        "crits_wanted": 21,
+        "pois_wanted": 21,
+        "known_zeros": [-47.038289673236127, -46.406755885040056],
+    }
 
 
 @pytest.fixture
-def analyzed_trig_func():
+def analyzed_trig_func(trig_func_args):
     """Fixture for an AnalyzedFunc describing trig_func."""
     analyzed_func = AnalyzedFunc(**trig_func_args)
     return analyzed_func
 
 
 @pytest.fixture
-def analyzed_trig_func_counted():
+def analyzed_trig_func_counted(trig_func_args):
     """Version of analyzed_trig_func with counted calls."""
     analyzed_func = AnalyzedFuncCounted(**trig_func_args)
     return analyzed_func
