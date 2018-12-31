d504bf20d88fb5ab4728773074ce93bb6967b186
Rohan Kumar <m2700879@nwytg.net>
2018-12-30 18:30:11 -0800

    Remove unused code for calculating errors.
    Generalize code for error analysis by adding calculate_error to
    testing_utils.

--- func_analysis/tests/test_area.py
+++ func_analysis/tests/test_area.py
@@ -1,33 +1,20 @@
 """Test area-finding methods."""
 
 from func_analysis.tests import constants
-
-
-def signed_area_error(analyzed_func, expected):
-    """Find error of an AnalyzedFunc object."""
-    actual = analyzed_func.signed_area
-    error = actual / expected
-    return abs(1 - error)
-
-
-def unsigned_area_error(analyzed_func, expected):
-    """Find error of an AnalyzedFunc object."""
-    actual = analyzed_func.unsigned_area
-    error = actual / expected
-    return abs(1 - error)
+from func_analysis.tests.testing_utils import calculate_error
 
 
 def test_trig_func_signed_area(analyzed_trig_func):
     """Test definite integral of trig_func is accurate."""
-    error = signed_area_error(
-        analyzed_trig_func, constants.TRIG_FUNC_SIGNED_AREA
+    error = calculate_error(
+        analyzed_trig_func.signed_area, constants.TRIG_FUNC_SIGNED_AREA
     )
     assert error < 5.81425e-5
 
 
 def test_trig_func_unsigned_area(analyzed_trig_func):
     """Test definite integral of trig_func is accurate."""
-    error = unsigned_area_error(
-        analyzed_trig_func, constants.TRIG_FUNC_UNSIGNED_AREA
+    error = calculate_error(
+        analyzed_trig_func.unsigned_area, constants.TRIG_FUNC_UNSIGNED_AREA
     )
-    assert error < 5.55566e-4
+    assert error < 5.55874e-4
--- func_analysis/tests/testing_utils.py
+++ func_analysis/tests/testing_utils.py
@@ -57,3 +57,8 @@ def assert_output_lessthan(func, x_vals, max_y):
     """Assert that func(x) < max_y for all x_vals."""
     y_vals = func(x_vals)
     assert np.amax(np.abs(y_vals)) < max_y
+
+
+def calculate_error(expected, actual):
+    """Calculate the experimental error."""
+    return abs(1 - actual / expected)
