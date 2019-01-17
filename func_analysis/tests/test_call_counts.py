f4ef04efcf0e18278012b0e74bd8cc34f7ce8647
Rohan Kumar <seirdy@protonmail.com>
2019-01-17 14:12:14 -0800

    Make test_call_counts a bit more agressive.
    Make test_call_counting forbid *any* repeated calls.

--- func_analysis/tests/test_call_counts.py
+++ func_analysis/tests/test_call_counts.py
@@ -16,10 +16,9 @@ def test_call_counting(analyzed_trig_func_counted):
     counts = workout_analyzed_func(analyzed_trig_func_counted)
     original_vals: Tuple[int, ...] = tuple(counts[0].values())
     deduped_vals: Tuple[int, ...] = tuple(counts[1].values())
-    uniqueness = np.divide(deduped_vals, original_vals)
-    # Ensure that memoized func isn't called again for repeat vals.
+    repeated_calls = np.subtract(original_vals, deduped_vals)
     counts_after_repeat = counts[0]["dupe"]
 
     assert original_vals.count(counts_after_repeat) > 1
-    assert counts_after_repeat < 1700
-    assert np.amin(uniqueness) > 0.8
+    assert counts_after_repeat < 1000
+    assert not repeated_calls.any()
